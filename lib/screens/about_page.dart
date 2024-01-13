8ec9a467ea4c376936eb2a7ae49621fd5fff327c
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-01-13 17:37:02 +0200

    some changes

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -25,6 +25,82 @@ class _AboutPageState extends State<AboutPage> {
           ),
         ),
       ),
+      body: SafeArea(
+        child: Column(
+          // Column
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage(
+                'assets/3d-hygge.png',
+              ),
+            ),
+            const Text(
+              'Ntwali 🇷🇼',
+              style: TextStyle(
+                fontSize: 40.0,
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+              ),
+            ),
+            const Text(
+              'SOFTWARE DEVELOPER 💻',
+              style: TextStyle(
+                fontFamily: 'MonoLisa',
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+                color: Colors.amber,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 250.0,
+              child: Divider(
+                color: Colors.amber.shade100,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 32.0,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+250 789-357 482',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                    fontSize: 18.0,
+                    fontFamily: 'MonoLisa',
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  size: 32.0,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'ntwalipit@gmail.com',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                    fontSize: 18.0,
+                    fontFamily: 'MonoLisa',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/help_page.dart
+++ lib/screens/help_page.dart
@@ -25,6 +25,138 @@ class _HelpPageState extends State<HelpPage> {
           ),
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Ufite Ikibazo cyangwa Igitekerezo',
+              style: TextStyle(
+                color: Colors.indigo[800],
+                fontSize: 16.0,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Duhamagare cyangwa Wohereze ubutumwa kuri:',
+              style: TextStyle(
+                color: Colors.indigo[800],
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+              width: double.infinity,
+              child: Divider(
+                color: Colors.indigo.shade100,
+              ),
+            ),
+            Text(
+              'Numero (Call, SMS na WhatsApp):',
+              style: TextStyle(
+                color: Colors.indigo[800],
+                fontSize: 16.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Card(
+              color: Colors.indigo[100],
+              elevation: 0,
+              margin: const EdgeInsets.symmetric(
+                vertical: 10.0,
+              ),
+              child: const ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 24.0,
+                  color: Colors.indigo,
+                ),
+                title: Text(
+                  '+250 789-357 482',
+                  style: TextStyle(
+                    color: Colors.indigo,
+                    fontSize: 16.0,
+                    fontFamily: 'MonoLisa',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Email:',
+              style: TextStyle(
+                color: Colors.indigo[800],
+                fontSize: 16.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Card(
+              color: Colors.indigo[100],
+              elevation: 0,
+              margin: const EdgeInsets.symmetric(
+                vertical: 10.0,
+              ),
+              child: const ListTile(
+                leading: Icon(
+                  Icons.email,
+                  size: 24.0,
+                  color: Colors.indigo,
+                ),
+                title: Text(
+                  'info@girlstechventures.com',
+                  style: TextStyle(
+                    color: Colors.indigo,
+                    fontSize: 16.0,
+                    fontFamily: 'MonoLisa',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Sura na Website:',
+              style: TextStyle(
+                color: Colors.indigo[800],
+                fontSize: 16.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Card(
+              color: Colors.indigo[100],
+              elevation: 0,
+              margin: const EdgeInsets.symmetric(
+                vertical: 10.0,
+              ),
+              child: const ListTile(
+                leading: Icon(
+                  Icons.web,
+                  size: 24.0,
+                  color: Colors.indigo,
+                ),
+                title: Text(
+                  'www.girlstechventures.com',
+                  style: TextStyle(
+                    color: Colors.indigo,
+                    fontSize: 16.0,
+                    fontFamily: 'MonoLisa',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

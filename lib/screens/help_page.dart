b13729d0c9e9aee575ce3849d8cb85a6167031f7
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-01-13 16:45:08 +0200

    some changes

--- lib/screens/help_page.dart
+++ lib/screens/help_page.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class HelpPage extends StatefulWidget {
@@ -10,16 +8,6 @@ class HelpPage extends StatefulWidget {
 }
 
 class _HelpPageState extends State<HelpPage> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _phoneNumberController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _messageController = TextEditingController();
-
-  Future sendEmail() async {
-    final Url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,100 +23,6 @@ class _HelpPageState extends State<HelpPage> {
             fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Nimba hari ibibazo bitasubijwe, cyangwa Wifuza kugira ikindi watubaza.\nUzuza iyi form. ',
-              style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.indigo[700]),
-            ),
-            const SizedBox(height: 15.0),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: _nameController,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.account_box),
-                      labelText: 'Amazina',
-                      hintText: '',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '⚠️ Andika Amazina yawe';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: _phoneNumberController,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.phone_android),
-                      labelText: 'Numero ya Telephone:',
-                      hintText: '',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '⚠️ Andika Numero yawe ya telephone ';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: _emailController,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.mail),
-                      labelText: 'Email yawe:',
-                      hintText: '',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '⚠️ Andika Email yawe';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: _messageController,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.message),
-                      labelText: 'Message',
-                    ),
-                    minLines: 5,
-                    maxLines: 8,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return '⚠️ Andika ubutumwa bwawe';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 15.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // Send the question with user details
-                        // ... implement logic to send data ...
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                              content: Text('Your question has been sent!')),
-                        );
-                      }
-                    },
-                    child: const Text('Ohereza Form'),
-                  ),
-                ],
-              ),
-            ),
-          ],
         ),
       ),
     );
